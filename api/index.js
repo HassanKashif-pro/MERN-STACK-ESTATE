1f2b87ee4e58ef7fede01069a8e62f9885e46df8
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-02-06 17:17:02 -0700

    create a test api--N.W

--- api/index.js
+++ api/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import userRouter from './api/routes/user.route.js';
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connect to MongoDB');
-}).catch((err) => {
-    console.log(err);
-})
+mongoose
+  .connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!!');
-    }
-);
+});
+
+app.use("/api/user",userRouter);
