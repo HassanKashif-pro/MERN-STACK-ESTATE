0aceb932c331165967da58e423e2d4d75c6228e3
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-02-13 17:20:44 -0700

    Create Signup api.

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
-import userRouter from './api/routes/user.route.js';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
   })
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!!');
 });
 
-app.use("/api/user",userRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
