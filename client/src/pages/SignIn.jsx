1e17caf0c5d438aaf2b08d2193d2715603e3446e
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-03-07 15:12:42 -0700

    Add redox tool kit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
+import { useDispatch, useSelector } from 'react-redox';
+import { signInStart, signInFailure, SignInSuccess } from '../redox/user/userSclice';
+import { signInSuccess } from '../redox/user/userSlice';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = userNavigate();
+  const dispatch = useDIspatch();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    dispatch(signInStart());
+    const res = await fetch('/api/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+    if(data.success === false) {
+      dispatch(signInFailure(data.message));
+      return;
+    }
+    dispatch(signInSuccess(data));
+    navigator('/')
+    console.log(data);
+  };
+  console.log(formData);
+
   return (
-    <div>
-      SignIn
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+        <button disbled={loading} type='submit' className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:placeholder-opacity-80'>{loading ? 'Loading...' : 'Sign In'}</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Don't Have an account?</p>
+        <Link to='/signup' className='text-blue-700'>Sign Up</Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
