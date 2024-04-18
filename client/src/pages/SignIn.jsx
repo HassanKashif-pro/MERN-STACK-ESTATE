e4fe0f1df62da35f5c60857babfe5648191d73c5
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-04-18 00:00:03 -0700

    Create and add Oauth component.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -19,7 +19,6 @@ export default function SignIn() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
     dispatch(signInStart());
     const res = await fetch('/api/auth/signin', {
       method: 'POST',
